b72a102185728c19a489da8907fdefc30197bc0e
Miley Wright <dishaliekwright@yahoo.com>
2020-01-28 23:44:55 -0500

    GET request /:id

--- accountRoutes.js
+++ accountRoutes.js
@@ -14,5 +14,17 @@ router.get('/', (req,res) => {
     })
 })
 
+// GET request /accounts
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db('accounts')
+    .where({'id': id})
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => {
+        res.status(500).json({error: 'can not retrieve account by ID'})
+    })
+})
 
 module.exports = router;
