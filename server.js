2555edfcda2876168c4079427177a3069a47bc02
Miley Wright <dishaliekwright@yahoo.com>
2020-01-28 21:43:59 -0500

    set up account route

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
-
 const server = express();
 
+const account = require('./accountRoutes');
+
 server.use(express.json());
+
+server.use('/accounts', account);
+
+server.get('/' , (req, res) => {
+    res.send(`
+    <h2> Welcome to my API </h2>`)
+})
 
 module.exports = server;
