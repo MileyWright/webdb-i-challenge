80583ed3b366a98591a3b0d5597d43696972605e
Miley Wright <dishaliekwright@yahoo.com>
2020-01-28 23:08:38 -0500

    GET request /

--- accountRoutes.js
+++ accountRoutes.js
@@ -1,5 +1,18 @@
 const express = require('express');
-const knex = require('./data/dbConfig.js');
+const db = require('./data/dbConfig.js');
 const router = express.Router();
+
+//GET request /accounts
+router.get('/', (req,res) => {
+    db('accounts')
+    .then(account => {
+        res.status(200).json(account)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error: 'Cannot retreive accounts'})
+    })
+})
+
 
 module.exports = router;
