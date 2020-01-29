375fd37ceaf3ee61a18d3233a25ff4bd126e394f
Miley Wright <dishaliekwright@yahoo.com>
2020-01-28 23:48:35 -0500

    placed empty CRUD

--- accountRoutes.js
+++ accountRoutes.js
@@ -27,4 +27,18 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+
+})
+
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
 module.exports = router;
